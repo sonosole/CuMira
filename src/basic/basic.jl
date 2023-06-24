f28c59534afcfa96106fe3e46d71b9114c415e5d
Sole <30993566+sonosole@users.noreply.github.com>
2023-06-25 01:21:34 +0800

    refactor: CuThreads and CuBlocks with 1/2/3-dims

--- src/basic/basic.jl
+++ src/basic/basic.jl
@@ -3,28 +3,84 @@ include("./CuArrayFun.jl")
 
 export CuBlocks
 export CuThreads
-export SetThreadsPerBlock
+export SetBlockSize
+
 
 # 每个 Block 块里拥有的线程数
-global ThreadsPerBlock = 512
+global XThreadsPerXBlock = 512
+global YThreadsPerYBlock = 512
+global ZThreadsPerZBlock = 32
 
-# 块数，取决于总任务数与块大小
-function CuBlocks(n::Int)
-    @assert n>=1 "minimum number of tasks is 1, but got $n"
-    return div(n + ThreadsPerBlock - 1, ThreadsPerBlock)
-end
-
-# 线程数，取决于最大 blocksize 与任务数
-function CuThreads(n::Int)
-    @assert n>=1 "minimum number of tasks is 1, but got $n"
-    return min(n, ThreadsPerBlock)
-end
 
 # 设置每个 block 的线程数量
-function SetThreadsPerBlock(n::Int)
-    @assert n>=1 "minimum number of threads is 1, but got $n"
-    global ThreadsPerBlock = n
+function SetBlockSize(x::Int)
+    @assert x≥1 "minimum number of threads is 1, but got $x"
+    global XThreadsPerBlock = x
     return nothing
+end
+
+function SetBlockSize(x::Int, y::Int)
+    @assert x≥1 "minimum number of threads is 1, but got $x"
+    @assert y≥1 "minimum number of threads is 1, but got $y"
+    global XThreadsPerBlock = x
+    global YThreadsPerBlock = y
+    return nothing
+end
+
+function SetBlockSize(x::Int, y::Int, z::Int)
+    @assert x≥1 "minimum number of threads is 1, but got $x"
+    @assert y≥1 "minimum number of threads is 1, but got $y"
+    @assert z≥1 "minimum number of threads is 1, but got $z"
+    global XThreadsPerBlock = x
+    global YThreadsPerBlock = y
+    global ZThreadsPerBlock = z
+    return nothing
+end
+
+
+# 线程数，取决于最大 blocksize 与任务数
+function CuThreads(x::Int)
+    @assert x≥1 "minimum number of tasks is 1, but got $x"
+    return min(x, XThreadsPerBlock)
+end
+
+function CuThreads(x::Int, y::Int)
+    @assert x≥1 "minimum number of tasks is 1, but got $x"
+    @assert y≥1 "minimum number of tasks is 1, but got $y"
+    return min(x, XThreadsPerBlock),
+           min(y, YThreadsPerBlock)
+end
+
+function CuThreads(x::Int, y::Int, z::Int)
+    @assert x≥1 "minimum number of tasks is 1, but got $x"
+    @assert y≥1 "minimum number of tasks is 1, but got $y"
+    @assert z≥1 "minimum number of tasks is 1, but got $z"
+    return min(x, XThreadsPerBlock),
+           min(y, YThreadsPerBlock),
+           min(z, ZThreadsPerBlock)
+end
+
+
+# 块数，取决于总任务数与块大小
+function CuBlocks(x::Int)
+    @assert x≥1 "minimum number of tasks is 1, but got $x"
+    return div(x + XThreadsPerBlock - 1, XThreadsPerBlock)
+end
+
+function CuBlocks(x::Int, y::Int)
+    @assert x≥1 "minimum number of tasks is 1, but got $x"
+    @assert y≥1 "minimum number of tasks is 1, but got $y"
+    return div(x + XThreadsPerBlock - 1, XThreadsPerBlock),
+           div(y + YThreadsPerBlock - 1, YThreadsPerBlock)
+end
+
+function CuBlocks(x::Int, y::Int, z::Int)
+    @assert x≥1 "minimum number of tasks is 1, but got $x"
+    @assert y≥1 "minimum number of tasks is 1, but got $y"
+    @assert z≥1 "minimum number of tasks is 1, but got $z"
+    return div(x + XThreadsPerBlock - 1, XThreadsPerBlock),
+           div(y + YThreadsPerBlock - 1, YThreadsPerBlock),
+           div(z + ZThreadsPerBlock - 1, ZThreadsPerBlock)
 end
 
 
